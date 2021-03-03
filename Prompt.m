22c0c41e0da5969bebbef34df29dfd0f6d8be9b1
bosolakhanathach <bosolakhanathach@localhost>
2021-03-03 00:34:05 -0500

    Added key press to start the game or review the instructions. Anyone know how to hide the figure window that pops up from the key press?

--- Prompt.m
+++ Prompt.m
@@ -22,46 +22,45 @@ fprintf('\n Enter corresponding number \n')
 fprintf('\n (1) Start the Game\n')
 fprintf('\n (2) Check the Rules\n')
 fprintf('\n           \n')
-player_input = input('');
+
+%Button press to determine their choice
+k = waitforbuttonpress;
+start.value = double(get(gcf,'CurrentCharacter'));
 
 
-
-%****************
-% Do we want to button press or enter in command window?
-%k = waitforbuttonpress;
-%value = double(get(gcf,'CurrentCharacter'))
-%if value == 50
-    %fprintf.....
 
 clc;
 % Enter number 1, run the game
-%       if player_input == 1
-%       blah blah blah%
+if start.value == 49
+main;
+end
 
-%Enter number 2, 
-if player_input == 2
+%Enter number 2, display rules
+%if player_input == 2
+if start.value == 50
     fprintf('\n   Rules for Egyptian Rat Screw:      \n')
     fprintf('\n         1. A player wins when their opponent loses all of their cards.      \n')
     fprintf('\n         2. Players take turns flipping cards into the center pile.  \n')
-    fprintf('\n            (Press ___ to flip cards. \n')
+    fprintf('\n            (Press ___ to flip cards.) \n')
     fprintf('\n         3. When a certain card or pattern of cards is played, the first player to "slap"  \n')
     fprintf('\n            the pile wins all the cards in the pile.\n')
-    fprintf('\n            Do so by pressing ___. \n')
+    fprintf('\n            (Press ___ to slap center pile.) \n')
     fprintf('\n                 Cards/Patterns to pay attention to: \n')
     fprintf('\n                      - Jacks \n')
     fprintf('\n                          - Anytime you see a Jack, slap.\n')
     fprintf('\n                      - Doubles \n')
-    fprintf('\n                          - Anytime you see a two of the same card, slap.\n')
-    fprintf('\n                          - For example: a four and a four\n')
-    fprintf('\n         4. If you slap incorrectly, you must "burn" – or lose – one of your cards \n')
+    fprintf('\n                          - Anytime you see two of the same card, slap.\n')
+    fprintf('\n                          - For example: a four then another four \n')
+    fprintf('\n         4. If you slap incorrectly, you must "burn" – or lose – one of your cards. \n')
     fprintf('\n           ')
     fprintf('\n         Good luck :) \n')
 
     fprintf('\n           ')
-    fprintf('\n         Enter any keys to return\n')  
+    fprintf('\n         Enter any key to start the game\n')  
 
     pause;
     clc;
+    main;
     
 end
 
