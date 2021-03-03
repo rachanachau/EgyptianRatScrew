013073cb1dd7d7553bd13ab7d7b679304128e981
Symone <43187077+symonehouston@users.noreply.github.com>
2021-03-03 00:15:42 -0500

    main added

--- computer_play.m
+++ computer_play.m
@@ -1,5 +1,6 @@
 function [computer_cards] = computer_play(deck1,computer_cards)
     %temp field 
     computer_cards = computer_cards - 1; 
+    
 end
 
---
+++ main.m
@@ -0,0 +1,5 @@
+player_cards = 0;
+computer_cards = 0; 
+Cards; 
+[deck1,deck2,computer_cards,player_cards] = hand_out_cards(computer_cards,player_cards); 
+[computer_cards] = computer_play(deck1,computer_cards); 
