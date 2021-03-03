ae4b87879808308c410bb9b35bc537d7f4f77f20
Jessica McDonald <jessica_mcdonald@brown.edu>
2021-03-03 11:53:18 -0500

    Modified hand_out_cards to have no input. This function will initialize the two deck sizes at 26 instead of having to initialize at 0 beforehand

--- hand_out_cards.m
+++ hand_out_cards.m
@@ -1,14 +1,18 @@
-function [deck1,deck2,computer_cards,player_cards] = hand_out_cards(computer_cards,player_cards)
+function [deck1,deck2,computer_cards,player_cards] = hand_out_cards()
 %HAND_OUT_CARDS: Returns two arrays of length 26 that collectively contain
 %all of the integers from 1 to 52 (shuffled)
 %(deck1 and deck2)
 %   Input: no input
 %   Output: deck1,deck2 - each an array of 26 integers, randomly chosen
-%   from an array of integers 1 through 52; there are no shared values
-%   between the two decks and no repeated values
+%           from an array of integers 1 through 52; there are no shared
+%           values between the two decks and no repeated values
+%           computer_cards - integer noting the number of cards the
+%           computer has
+%           player_cards - integer noting the number of cards the
+%           player has
 full_deck = randperm(52);
 deck1 = full_deck(1:26);
 deck2 = full_deck(27:52);
-player_cards = 52/2;
-computer_cards = 52/2; 
+player_cards = length(deck1);
+computer_cards = length(deck2); 
 end
--- main.m
+++ main.m
@@ -1,6 +1,11 @@
-player_cards = 0;
-computer_cards = 0;
+%player_cards = 0;
+%computer_cards = 0;
+%modified hand_out_cards to have no input; output will initialize number of
+%cards each person has (26 to start)
+%leaving it as comment now in case you want to initialize at 0, but I
+%figured that might cause problems in our while loop if we have it run
+%while neither equals 0
 center_cards = []; 
 Cards; 
-[deck1,deck2,computer_cards,player_cards] = hand_out_cards(computer_cards,player_cards); 
+[deck1,deck2,computer_cards,player_cards] = hand_out_cards(); 
 [computer_cards,center_cards] = computer_play(deck1,computer_cards,center_cards,CardDeckPictures); 
