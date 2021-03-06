5ff2bf3632c15ee1a18a8aaca57b392df29a0a52
Jessica McDonald <jessica_mcdonald@brown.edu>
2021-03-06 18:01:27 -0500

    cleaned up comments

--- main.m
+++ main.m
@@ -1,12 +1,3 @@
-%player_cards = 0;
-%computer_cards = 0;
-%modified hand_out_cards to have no input; output will initialize number of
-%cards each person has (26 to start)
-%leaving it as comment now in case you want to initialize at 0, but I
-%figured that might cause problems in our while loop if we have it run
-%while neither equals 0
-%= containers.Map('KeyType','double','ValueType','char');
- 
 global cardDeckNum cardDeckSuite cardDeckName valid_slap cardDeckPictures center_cards deck1 deck2 computer_cards player_cards pseudo_center;
 center_cards = []; 
 pseudo_center = []; 
