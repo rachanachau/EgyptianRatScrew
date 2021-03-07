cdc2340c8ed7bfd90da0a0ab0cd8293ad6cfae41
Symone <43187077+symonehouston@users.noreply.github.com>
2021-03-07 17:31:27 -0500

    losing images

--- end_game.m
+++ end_game.m
@@ -1,12 +1,26 @@
-pictures = ["win1.jpg", "win2.jpg", "win3.jpg", "win4.jpg", "win5.jpg", "win6.jpg"];
-
-figure(1);
-clf;
-for ii=1:6
-    pull = pictures(ii)
-    src = strcat('win/',pull);
-    imread(src);
-    imshow(src);
-    axis('image');
-    axis off
-end 
+win = ["win1.jpg", "win2.jpg", "win3.jpg", "win4.jpg", "win5.jpg", "win6.jpg"];
+lose = ["lose1.jpg", "lose2.jpg", "lose3.jpg", "lose4.jpg", "lose5.jpg", "lose6.jpg", "lose7.jpg","lose8.jpg"];
+player_cards = 3; 
+if player_cards == 0
+    figure(1);
+    clf;
+    for ii=1:6
+        pull = pictures(ii)
+        src = strcat('win/',pull);
+        imread(src);
+        imshow(src);
+        axis('image');
+        axis off
+    end 
+else
+    figure(1);
+    clf;
+    for ii=1:8
+        pull = lose(ii)
+        src = strcat('win/',pull);
+        imread(src);
+        imshow(src);
+        axis('image');
+        axis off
+    end 
+end
